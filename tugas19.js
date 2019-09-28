6ddf285fd4fa9d5d873ca2d74969cf574a1ef4f6
Anshor <anshorjihadi@gmail.com>
2019-09-28 11:03:27 +0700

    Tugas 19 Js

---
+++ tugas19.js
@@ -0,0 +1,11 @@
+console.log("Semangat...".repeat(3));
+console.log("");
+console.log("*".repeat(5));
+console.log("*".repeat(25));
+console.log("*".repeat(50));
+console.log("*".repeat(75));
+console.log("*".repeat(100));
+console.log("*".repeat(75));
+console.log("*".repeat(50));
+console.log("*".repeat(25));
+console.log("*".repeat(5));
